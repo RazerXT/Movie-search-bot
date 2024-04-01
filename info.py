3e355c0a0d52f900352a7b47d78ac80350f8e1ac
RazerXT <101035245+RazerXT@users.noreply.github.com>
2024-04-01 14:39:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,8 +49,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'earnpro.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '1ab8c8c9b9e12a952ea920b3814768e45b975449')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'adrinolinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '4d6e2da72dd65ec8465a828eb2857f070c03f12f')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
